8c954a4b36a4e545ef68191fd435958323ad84e8
noah_huang <smhuang426@gmail.com>
2015-08-29 04:11:36 +0000

    [HC_mysql] mysql lib change

--- Library/Mysql/Ex_mysql.c
+++ Library/Mysql/Ex_mysql.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void finish_with_error(MYSQL *con);
 
@@ -34,6 +35,76 @@ void finish_with_error(MYSQL *con)
 
 	exit(1);     
 
+}
+
+void ex_mysql_list_db(MYSQL *con)
+{
+	if (mysql_query(con, "show databases")) 
+  	{
+      		finish_with_error(con);
+  	}
+
+  	MYSQL_RES *result = mysql_store_result(con);
+  
+  	if (result == NULL) 
+  	{
+      		finish_with_error(con);
+  	}
+
+  	int num_fields = mysql_num_fields(result);
+
+  	MYSQL_ROW row;
+	int i = 0;
+
+	printf("[ex_mysql_list_db]there are %d DBs as following below:\n"); 
+  	while ((row = mysql_fetch_row(result))) 
+  	{ 
+      		for(i = 0; i < num_fields; i++) 
+      		{ 
+          		printf("	%s ", row[i] ? row[i] : "NULL"); 
+      		} 
+          		printf("\n"); 
+  	}
+	printf("[ex_mysql_list_db]end of db look up\n");
+
+  	mysql_free_result(result);
+}
+
+
+EX_MYSQL_RES ex_mysql_is_db_exist(MYSQL *con, char* db_name)
+{
+	EX_MYSQL_RES ret = EX_MYSQL_FAIL;
+
+	if (mysql_query(con,"show databases")) 
+  	{
+      		return ret;
+  	}
+
+  	MYSQL_RES *result = mysql_store_result(con);
+  
+  	if (result == NULL) 
+  	{
+		return ret;
+  	}
+
+  	int num_fields = mysql_num_fields(result);
+
+  	MYSQL_ROW row;
+	int i = 0;
+
+  	while ((row = mysql_fetch_row(result))) 
+  	{ 
+      		for(i = 0; i < num_fields; i++) 
+      		{ 
+          		if ((strcmp( row[i] , db_name)==0))
+			{
+				return EX_MYSQL_SUCCESS;
+			} 
+      		} 
+  	}
+
+  	mysql_free_result(result);
+	return ret;
 }
 
 
--- Library/Mysql/Ex_mysql.h
+++ Library/Mysql/Ex_mysql.h
@@ -6,6 +6,15 @@
 #define EX_USER		"root"
 #define EX_PASSWORD	"OK5074"
 
+typedef enum _EX_MYSQL_RES
+{
+	EX_MYSQL_SUCCESS = 0,
+	EX_MYSQL_FAIL
+}  EX_MYSQL_RES;
+
+
 MYSQL *ex_mysql_init();
+void ex_mysql_list_db(MYSQL *con);
+EX_MYSQL_RES ex_mysql_is_db_exist(MYSQL *con, char* db_name);
 
 #endif
