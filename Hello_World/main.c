975e905a496dea899f1f7aa520b88c831411573f
NoaSMHuangh <NoahSMHuang@fih-foxconn.com>
2015-09-12 21:52:45 +0800

    [HC] commit for hello world code test
    Symptom: NA
    Root Cause: NA
    Solution: commit for helloe world code test
    Project: HC
    Note: NA

--- Hello_World/main.c
+++ Hello_World/main.c
@@ -1,4 +1,4 @@
-#include <wiringPi.h>
+//#include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -11,10 +11,76 @@ pthread_t thr1,thr2;
 /*declare function*/
 void *thread_function(void* ptr);
 
+int function (int from , int to);
+
+int a[4]={0,1,1,1};
+
+typedef enum BOOL{
+    false = 0,
+    true  = 1
+} BOOL;
+
+int isBadVersion(int version)
+{
+    if (a[version] == 1)
+    {
+        return 1;
+    }
+    
+    else{
+        return 0;
+    }
+}
+
+int firstBadVersion(int n) {
+    
+    if (isBadVersion(1) == true)    return 1;
+    if (isBadVersion(n) == true)    return n;
+    
+    return function(1,n);
+}
+
+int function (int from , int to){
+    
+    int _temp = to - from;
+    int temp = _temp/2 + from;
+    BOOL last_chance = false;
+    int ret = 0;
+    
+    if (_temp <= 2)
+    {
+        last_chance = true;
+    }
+    
+    if (isBadVersion(temp) != true){
+        
+        if (last_chance == true){
+            ret = to;
+        }else{
+        
+            function (from , temp);
+        }
+        
+    }else{
+        
+        if (last_chance == true){
+            ret = temp;
+        }else{
+        
+            function (temp, to);
+        }
+    }
+    
+    return ret;
+    
+}
+
 int main()
 {
 	int ret = 0;
 	char* str1="test1",*str2="test2";
+    
+    printf("ret:%d\n",/*firstBadVersion(3)*/(int)1.5 );
 
 	printf("hello world\n");
 
@@ -45,5 +111,7 @@ void *thread_function(void* ptr)
 	char *str;
 	str = (char *)ptr;
 	printf("pthread_t:%s\n",str);
+    
+    return 0;
 }
 
