5edaa320f0e4ce686eb4607be7c2c613a32321e3
NoaSMHuangh <smhuang426@gmail.com>
2015-11-23 00:26:27 +0800

    [HC] add 2D kalman algorithm
    Symptom: not finish yet
    Root Cause: NA
    Solution: add 2D kalman algorithm
    Project: HC
    Note: NA

--- Library/Kalman_filter/kalman.c
+++ Library/Kalman_filter/kalman.c
@@ -51,3 +51,65 @@ void KM_1d_set_R(KM_1d_config* _config, double _R)
     _config->R = _R;
 }
 
+/*matrix calculate*/
+double **double_array_malloc(int row, int col)
+{
+    double** array = (double**)malloc(col*sizeof(double*));
+    array[0] = (double*)malloc(row*col*sizeof(double));
+    
+    int r_index = 0,c_index = 0;
+    
+    for (c_index = 0;c_index<col;c_index++)
+    {
+        array[c_index] = (double*)(*array + (row*c_index));
+    }
+    
+    for (c_index = 0; c_index<col; c_index++) {
+        for (r_index = 0; r_index<row; r_index++) {
+            array[c_index][r_index] = 0;
+        }
+    }
+    
+    return array;
+}
+
+double* array_malloc(int size)
+{
+    double *array = (double*)calloc(size*sizeof(double));
+    
+    return array;
+}
+
+/*2D kalman filter for constant varity*/
+void KM_2d_init(KM_1d_config* _config,double _R, double _Q, double delta_t)
+{
+    _config->current_output = array_malloc(2);
+    _config->previous_output = array_malloc(2);
+    
+    _config->F = double_array_malloc(2, 2);
+    _config->F[0][0] = _config->F[1][1] = 1;
+    _config->F[0][1] = delta_t;
+    
+    _config->H = array_malloc(2);
+    _config->H[0] = 1;
+    
+    _config->current_estimate = double_array_malloc(2, 2);
+    _config->current_estimate[0][1] = _config_->current_estimate[1][0] = 1000;
+    
+    _config->next_estimate = double_array_malloc(2, 2);
+    
+    _config->R = _R;
+    
+    _config->Q = double_array_malloc(2, 2);
+    _config->Q[0][1] = _config->Q[1][0] = pow(delta_t,2.0)*0.5*_Q;
+    _config->Q[0][0] = pow(delta_t,3.0)*0.3333333333*_Q;
+    _config->Q[1][1] = delta_t*_Q;
+    
+    _config->KM_gain = array_malloc(2);
+    
+}
+
+double KM_2d_start(KM_1d_config* _config, double measure);
+void KM_2d_set_Q(KM_1d_config* _config, double _Q);
+void KM_2d_set_R(KM_1d_config* _config, double _R);
+
--- Library/Kalman_filter/kalman.h
+++ Library/Kalman_filter/kalman.h
@@ -17,11 +17,32 @@ typedef struct KM_1d_config{
     
 } KM_1d_config;
 
+typedef struct KM_2d_config{
+    
+    double* current_output;
+    double* previous_output;
+    double** F;
+    double* H;
+    double** current_estimate;
+    double** next_estimate;
+    double R;
+    double** Q;
+    double* KM_gain;
+    
+} KM_2d_config;
+
 
 /*global function define*/
 void KM_1d_init(KM_1d_config* _config,double _R, double _Q);
 double KM_1d_start(KM_1d_config* _config, double measure);
 void KM_1d_set_Q(KM_1d_config* _config, double _Q);
 void KM_1d_set_R(KM_1d_config* _config, double _R);
+
+/*2D kalman filter for constant varity*/
+void KM_2d_init(KM_1d_config* _config,double _R, double _Q);
+double KM_2d_start(KM_1d_config* _config, double measure);
+void KM_2d_set_Q(KM_1d_config* _config, double _Q);
+void KM_2d_set_R(KM_1d_config* _config, double _R);
+
 
 #endif
